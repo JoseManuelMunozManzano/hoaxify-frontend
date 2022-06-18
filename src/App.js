1bed0f28f453d1299ce7e2e29e84dc7bdebf56a5
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-06-18 05:38:02 +0200

    User Signup - Sending Requests to Backend - Green flag and proxy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import { UserSignUpPage } from './pages/UserSignupPage.spec';
+import * as apiCalls from './api/apiCalls';
+
+// Para pasar la función sign-up como un prop a UserSignupPage
+// Esto no quedará en producción, es solo con propósito de pruebas.
+// Esto lo hacemos aquí porque más tarde vamos a meter Redux y se dispararán las acciones de Sign Up a través de el.
+const actions = {
+  postSignup: apiCalls.signup,
+};
 
 function App() {
-  return <UserSignUpPage />;
+  return <UserSignUpPage actions={actions} />;
 }
 
 export default App;
--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,3 +1,7 @@
 // Este fuente contiene las funciones para realizar peticiones API al exterior.
+import axios from 'axios';
 
-export const signup = () => {};
+export const signup = (user) => {
+  // Aquí tenemos la primera llamada y el primer parámetro es el path
+  return axios.post('/api/1.0/users', user);
+};
