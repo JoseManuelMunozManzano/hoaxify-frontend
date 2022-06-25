512b6b851363711f106fce3c6f23159b5461d032
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-06-25 13:30:51 +0200

    Client Internal Routing - React Router - Segunda parte LoginPage

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,13 @@
 import { Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
+import LoginPage from '../pages/LoginPage';
 
 function App() {
   return (
     <div>
       <div className="container">
         <Route path="/" component={HomePage} />
+        <Route path="/login" component={LoginPage} />
       </div>
     </div>
   );
--- src/containers/App.spec.js
+++ src/containers/App.spec.js
@@ -1,17 +1,24 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
+const setup = (path) => {
+  return render(
+    <MemoryRouter initialEntries={[path]}>
+      <App />
+    </MemoryRouter>
+  );
+};
+
 describe('App', () => {
   it('displays homepage when url is /', () => {
-    // Para renderizar App necesitamos establecer los paths. Necesitamos MemoryRouter.
-    // Hay que configurar la propiedad initialEntries, que toma un array con el historial de paths.
-    const { queryByTestId } = render(
-      <MemoryRouter initialEntries={['/']}>
-        <App />
-      </MemoryRouter>
-    );
+    const { getByTestId } = setup('/');
+    expect(screen.getByTestId('homepage')).toBeInTheDocument();
+  });
 
-    expect(queryByTestId('homepage')).toBeInTheDocument();
+  it('displays LoginPage when url is /login', () => {
+    const { container } = setup('/login');
+    const header = container.querySelector('h1');
+    expect(header).toHaveTextContent('Login');
   });
 });
