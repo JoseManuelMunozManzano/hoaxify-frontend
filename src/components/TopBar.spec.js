8945af30e91a38ff1493f483050257fa10199238
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-06-26 06:03:26 +0200

    Client State Management - Redux - Añadido en test

--- src/components/TopBar.spec.js
+++ src/components/TopBar.spec.js
@@ -2,13 +2,39 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import TopBar from './TopBar';
 import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import authReducer from '../redux/authReducer';
+
+const loggedInState = {
+  id: 1,
+  username: 'user1',
+  displayName: 'display1',
+  image: 'profile1.png',
+  password: 'P4ssword',
+  isLoggedIn: true,
+};
+
+const defaultState = {
+  id: 0,
+  username: '',
+  displayName: '',
+  image: '',
+  password: '',
+  isLoggedIn: false,
+};
 
 // Para evitar el error: You should not use <Link> outside a <Router>
+// Se añade Redux
 const setup = () => {
+  const store = createStore(authReducer);
+
   return render(
-    <MemoryRouter>
-      <TopBar />
-    </MemoryRouter>
+    <Provider store={store}>
+      <MemoryRouter>
+        <TopBar />
+      </MemoryRouter>
+    </Provider>
   );
 };
 
