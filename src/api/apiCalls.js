9f1997cd9551b4104db15888be5961fbb72cf8f9
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-07-09 06:15:17 +0200

    Listing Users - Obtener usuarios usando page o size

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -23,6 +23,6 @@ export const setAuthorizationHeader = ({ username, password, isLoggedIn }) => {
 };
 
 export const listUsers = (param = { page: 0, size: 3 }) => {
-  const path = `/api/1.0/users?page=${param.page}&size=${param.size}`;
+  const path = `/api/1.0/users?page=${param.page || 0}&size=${param.size || 3}`;
   return axios.get(path);
 };
