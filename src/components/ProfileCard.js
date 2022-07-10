b19b82fb51e5c58c4884a495b86e4e643f72345c
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-07-10 16:39:16 +0200

    User Profile - ProfileCard refactor 2

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const ProfileCard = (props) => {
   const { displayName, username } = props.user;
-  return <div>{`${displayName}@${username}`}</div>;
+  return (
+    <div className="card">
+      <div className="card-body text-center">
+        <h4>{`${displayName}@${username}`}</h4>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileCard;
--- src/components/ProfileCard.spec.js
+++ src/components/ProfileCard.spec.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ProfileCard from './ProfileCard';
 
+const user = {
+  id: 1,
+  username: 'user1',
+  displayName: 'display1',
+  image: 'profile1.png',
+};
+
 describe('ProfileCard', () => {
   describe('Layout', () => {
     it('displays the displayName@username', () => {
-      const user = {
-        id: 1,
-        username: 'user1',
-        displayName: 'display1',
-        image: 'profile1.png',
-      };
-
       const { queryByText } = render(<ProfileCard user={user} />);
       const userInfo = screen.queryByText('display1@user1');
       expect(userInfo).toBeInTheDocument();
