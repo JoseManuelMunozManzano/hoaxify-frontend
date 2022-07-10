17446785599bcaa6ed6628f1458d0212e5d7d864
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-07-10 06:10:24 +0200

    User Profile - Lifecycle - Llamar a getUser cuando UserPage se renderiza

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import * as apiCalls from '../api/apiCalls';
 
 class UserPage extends React.Component {
+  componentDidMount() {
+    apiCalls.getUser();
+  }
+
   render() {
     return <div data-testid="userpage">Userpage</div>;
   }
--- src/pages/UserPage.spec.js
+++ src/pages/UserPage.spec.js
@@ -1,5 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import UserPage from './UserPage';
+import * as apiCalls from '../api/apiCalls';
+
+const mockSuccessGetUser = {
+  data: {
+    id: 1,
+    username: 'user1',
+    displayName: 'display1',
+    image: 'profile1.png',
+  },
+};
+
+// Con match estamos replicando lo que se ve en Chrome, en Developer Tools, pestaña Components, y buscar UserPage y click en el.
+// En la parte derecha, en props, se ve este match con el parámetro params y username
+const match = {
+  params: {
+    username: 'user1',
+  },
+};
 
 describe('UserPage', () => {
   describe('Layout', () => {
@@ -11,6 +29,14 @@ describe('UserPage', () => {
       const { queryByTestId } = render(<UserPage />);
       const userPageDiv = screen.queryByTestId('userpage');
       expect(userPageDiv).toBeInTheDocument();
+    });
+  });
+
+  describe('Lifecycle', () => {
+    it('calls getUser when it is rendered', () => {
+      apiCalls.getUser = jest.fn().mockResolvedValue(mockSuccessGetUser);
+      render(<UserPage match={match} />);
+      expect(apiCalls.getUser).toHaveBeenCalledTimes(1);
     });
   });
 });
