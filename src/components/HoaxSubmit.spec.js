a68ddcdf22e440b976261df06c197178429a794e
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-07-23 05:48:31 +0200

    Hoax Submit - Refactor para usar redux

--- src/components/HoaxSubmit.spec.js
+++ src/components/HoaxSubmit.spec.js
@@ -1,23 +1,50 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import HoaxSubmit from './HoaxSubmit';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import authReducer from '../redux/authReducer';
+
+const defaultState = {
+  id: 1,
+  username: 'user1',
+  displayName: 'display1',
+  image: 'profile1.png',
+  password: 'P4ssword',
+  isLoggedIn: true,
+};
+
+let store;
+
+// Para evitar el error: You should not use <Link> outside a <Router>
+// Se añade Redux
+// Se añade state. Como no todos los tests llaman a setup con parámetro se pone un valor por defecto
+const setup = (state = defaultState) => {
+  store = createStore(authReducer, state);
+
+  return render(
+    <Provider store={store}>
+      <HoaxSubmit />
+    </Provider>
+  );
+};
 
 describe('HoaxSubmit', () => {
   describe('Layout', () => {
     it('has textarea', () => {
-      const { container } = render(<HoaxSubmit />);
+      const { container } = setup();
       const textArea = container.querySelector('textarea');
       expect(textArea).toBeInTheDocument();
     });
 
     it('has image', () => {
-      const { container } = render(<HoaxSubmit />);
+      const { container } = setup();
       const image = container.querySelector('img');
       expect(image).toBeInTheDocument();
     });
 
     it('displays textarea 1 line', () => {
-      const { container } = render(<HoaxSubmit />);
+      const { container } = setup();
       const textArea = container.querySelector('textarea');
       expect(textArea.rows).toBe(1);
     });
