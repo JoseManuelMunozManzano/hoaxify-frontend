5d896380cae1abfe1252402d61492561b39bd80c
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-07-24 06:21:17 +0200

    Hoax Feed - Refactor

--- src/components/HoaxFeed.spec.js
+++ src/components/HoaxFeed.spec.js
@@ -3,23 +3,33 @@ import { render } from '@testing-library/react';
 import HoaxFeed from './HoaxFeed';
 import * as apiCalls from '../api/apiCalls';
 
+const setup = (props) => {
+  return render(<HoaxFeed {...props} />);
+};
+
+const mockEmptyResponse = {
+  data: {
+    content: [],
+  },
+};
+
 describe('HoaxFeed', () => {
   describe('Lifecycle', () => {
     it('calls loadHoaxes when it is rendered', () => {
-      apiCalls.loadHoaxes = jest.fn().mockResolvedValue({});
-      render(<HoaxFeed />);
+      apiCalls.loadHoaxes = jest.fn().mockResolvedValue(mockEmptyResponse);
+      setup();
       expect(apiCalls.loadHoaxes).toHaveBeenCalled();
     });
 
     it('calls loadHoaxes with user parameter when it is rendered with user property', () => {
-      apiCalls.loadHoaxes = jest.fn().mockResolvedValue({});
-      render(<HoaxFeed user="user1" />);
+      apiCalls.loadHoaxes = jest.fn().mockResolvedValue(mockEmptyResponse);
+      setup({ user: 'user1' });
       expect(apiCalls.loadHoaxes).toHaveBeenCalledWith('user1');
     });
 
     it('calls loadHoaxes without user parameter when it is rendered without user property', () => {
-      apiCalls.loadHoaxes = jest.fn().mockResolvedValue({});
-      render(<HoaxFeed />);
+      apiCalls.loadHoaxes = jest.fn().mockResolvedValue(mockEmptyResponse);
+      setup();
       const parameter = apiCalls.loadHoaxes.mock.calls[0][0];
       expect(parameter).toBeUndefined();
     });
