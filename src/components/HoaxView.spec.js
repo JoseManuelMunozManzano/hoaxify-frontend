e4f1c56eddf479eccfebdd8ce01675de7aa6f94d
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-07-24 07:35:36 +0200

    Hoax Feed - Refactor test HoaxView.spec

--- src/components/HoaxView.spec.js
+++ src/components/HoaxView.spec.js
@@ -2,21 +2,25 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import HoaxView from './HoaxView';
 
+const setup = () => {
+  const hoax = {
+    id: 10,
+    content: 'This is the first hoax',
+    date: 1561294668539,
+    user: {
+      id: 1,
+      username: 'user1',
+      displayName: 'display1',
+      image: 'profile1.png',
+    },
+  };
+  return render(<HoaxView hoax={hoax} />);
+};
+
 describe('HoaxView', () => {
   describe('Layout', () => {
     it('displays hoax content', () => {
-      const hoax = {
-        id: 10,
-        content: 'This is the first hoax',
-        date: 1561294668539,
-        user: {
-          id: 1,
-          username: 'user1',
-          displayName: 'display1',
-          image: 'profile1.png',
-        },
-      };
-      render(<HoaxView hoax={hoax} />);
+      setup();
       expect(screen.queryByText('This is the first hoax')).toBeInTheDocument();
     });
   });
