e90d8b85ca3a9daa8a805789e47b5012317bea0c
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-07-31 07:21:26 +0200

    UserList con hooks, useEffect y useCallback

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,85 +1,79 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import * as apiCalls from '../api/apiCalls';
 import UserListItem from './UserListItem';
 
-// En este componente listaremos los usuarios. Tendrá su propio estado y hará llamadas a la API
-class UserList extends React.Component {
-  state = {
-    page: {
-      content: [],
-      number: 0,
-      size: 3,
+const UserList = (props) => {
+  const [page, setPage] = useState({
+    content: [],
+    number: 0,
+    size: 3,
+  });
+  const [loadError, setLoadError] = useState();
+
+  const loadData = useCallback(
+    (requestedPage = 0) => {
+      apiCalls
+        .listUsers({ page: requestedPage, size: page.size })
+        .then((response) => {
+          setPage(response.data);
+          setLoadError();
+        })
+        .catch((error) => {
+          setLoadError('User load failed');
+        });
     },
+    [page.size]
+  );
+
+  useEffect(() => {
+    loadData();
+  }, [loadData]);
+
+  const onClickNext = () => {
+    loadData(page.number + 1);
   };
 
-  // Ciclo de Vida: llamado cuando este componente se añada a la Page
-  componentDidMount() {
-    this.loadData();
-  }
-
-  loadData = (requestedPage = 0) => {
-    apiCalls
-      .listUsers({ page: requestedPage, size: this.state.page.size })
-      .then((response) => {
-        this.setState({
-          page: response.data,
-          loadError: undefined,
-        });
-      })
-      .catch((error) => {
-        this.setState({
-          loadError: 'User load failed',
-        });
-      });
+  const onClickPrevious = () => {
+    loadData(page.number - 1);
   };
 
-  onClickNext = () => {
-    this.loadData(this.state.page.number + 1);
-  };
-
-  onClickPrevious = () => {
-    this.loadData(this.state.page.number - 1);
-  };
-
-  render() {
-    return (
-      <div className="card">
-        <h3 className="card-title m-auto">Users</h3>
-        <div className="list-group list-group-flush" data-testid="usergroup">
-          {this.state.page.content.map((user) => {
-            return <UserListItem key={user.username} user={user} />;
-          })}
-        </div>
-        <div className="clearfix">
-          {!this.state.page.first && (
-            <span
-              data-testid="previous"
-              className="badge badge-light float-left"
-              style={{ cursor: 'pointer' }}
-              onClick={this.onClickPrevious}
-            >
-              {`< previous`}
-            </span>
-          )}
-          {!this.state.page.last && (
-            <span
-              data-testid="next"
-              className="badge badge-light float-right"
-              style={{ cursor: 'pointer' }}
-              onClick={this.onClickNext}
-            >
-              next {'>'}
-            </span>
-          )}
-        </div>
-        {this.state.loadError && (
-          <span data-testid="load-failed" className="text-center text-danger">
-            {this.state.loadError}
+  return (
+    <div className="card">
+      <h3 className="card-title m-auto">Users</h3>
+      <div className="list-group list-group-flush" data-testid="usergroup">
+        {page.content.map((user) => {
+          return <UserListItem key={user.username} user={user} />;
+        })}
+      </div>
+      <div className="clearfix">
+        {!page.first && (
+          <span
+            data-testid="previous"
+            className="badge badge-light float-left"
+            style={{ cursor: 'pointer' }}
+            onClick={onClickPrevious}
+          >
+            {`< previous`}
+          </span>
+        )}
+        {!page.last && (
+          <span
+            data-testid="next"
+            className="badge badge-light float-right"
+            style={{ cursor: 'pointer' }}
+            onClick={onClickNext}
+          >
+            next {'>'}
           </span>
         )}
       </div>
-    );
-  }
-}
+      {loadError && (
+        <span data-testid="load-failed" className="text-center text-danger">
+          {loadError}
+        </span>
+      )}
+    </div>
+  );
+};
 
 export default UserList;
