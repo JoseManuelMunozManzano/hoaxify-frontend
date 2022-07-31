053efb388d09ea855b54560f35ea97e49c814232
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-07-31 07:02:26 +0200

    UserSignupPage con hooks

--- src/pages/UserSignupPage.js
+++ src/pages/UserSignupPage.js
@@ -1,128 +1,129 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonWithProgress from '../components/ButtonWithProgress';
 import Input from '../components/Input';
 import { connect } from 'react-redux';
 import * as authActions from '../redux/authActions';
 
-export class UserSignUpPage extends React.Component {
-  state = {
+export const UserSignUpPage = (props) => {
+  const [form, setForm] = useState({
     displayName: '',
     username: '',
     password: '',
     passwordRepeat: '',
-    pendingApiCall: false,
-    errors: {},
-  };
+  });
+  const [errors, setErrors] = useState({});
+  const [pendingApiCall, setPendingApiCall] = useState(false);
 
-  onChange = (event) => {
-    const value = event.target.value;
-    const name = event.target.name;
+  const onChange = (event) => {
+    const { value, name } = event.target;
 
-    const errors = { ...this.state.errors };
-    delete errors[name];
+    setForm((previousForm) => {
+      return {
+        ...previousForm,
+        [name]: value,
+      };
+    });
 
-    this.setState({
-      [name]: value,
-      errors,
+    setErrors((previousErrros) => {
+      return {
+        ...previousErrros,
+        [name]: undefined,
+      };
     });
   };
 
-  onClickSignup = () => {
+  const onClickSignup = () => {
     const user = {
-      username: this.state.username,
-      displayName: this.state.displayName,
-      password: this.state.password,
+      username: form.username,
+      displayName: form.displayName,
+      password: form.password,
     };
-    this.setState({ pendingApiCall: true });
-    this.props.actions
+    setPendingApiCall(true);
+
+    props.actions
       .postSignup(user)
       .then((response) => {
-        this.setState({ pendingApiCall: false }, () => {
-          this.props.history.push('/');
-        });
+        setPendingApiCall(false);
+        props.history.push('/');
       })
       .catch((apiError) => {
-        // console.log(JSON.stringify(apiError.response.data));
-        let errors = { ...this.state.errors };
         if (apiError.response.data && apiError.response.data.validationErrors) {
-          errors = { ...apiError.response.data.validationErrors };
+          setErrors(apiError.response.data.validationErrors);
         }
-        this.setState({ pendingApiCall: false, errors });
+        setPendingApiCall(false);
       });
   };
 
-  render() {
-    let passwordRepeatError;
-    const { password, passwordRepeat } = this.state;
-    if (password || passwordRepeat) {
-      passwordRepeatError = password === passwordRepeat ? '' : 'Does not match to password';
-    }
-
-    return (
-      <div className="container">
-        <h1 className="text-center">Sign Up</h1>
-        <div className="col-12 mb-3">
-          <Input
-            name="displayName"
-            label="Display Name"
-            placeholder="Your display name"
-            value={this.state.displayName}
-            onChange={this.onChange}
-            hasError={this.state.errors.displayName && true}
-            error={this.state.errors.displayName}
-          />
-        </div>
-
-        <div className="col-12 mb-3">
-          <Input
-            name="username"
-            label="Username"
-            placeholder="Your username"
-            value={this.state.username}
-            onChange={this.onChange}
-            hasError={this.state.errors.username && true}
-            error={this.state.errors.username}
-          />
-        </div>
-
-        <div className="col-12 mb-3">
-          <Input
-            name="password"
-            label="Password"
-            placeholder="Your password"
-            type="password"
-            value={this.state.password}
-            onChange={this.onChange}
-            hasError={this.state.errors.password && true}
-            error={this.state.errors.password}
-          />
-        </div>
-
-        <div className="col-12 mb-3">
-          <Input
-            name="passwordRepeat"
-            label="Password Repeat"
-            placeholder="Repeat your password"
-            type="password"
-            value={this.state.passwordRepeat}
-            onChange={this.onChange}
-            hasError={passwordRepeatError && true}
-            error={passwordRepeatError}
-          />
-        </div>
-
-        <div className="text-center">
-          <ButtonWithProgress
-            onClick={this.onClickSignup}
-            disabled={this.state.pendingApiCall || passwordRepeatError ? true : false}
-            pendingApiCall={this.state.pendingApiCall}
-            text="Sign Up"
-          />
-        </div>
-      </div>
-    );
+  let passwordRepeatError;
+  const { password, passwordRepeat } = form;
+  if (password || passwordRepeat) {
+    passwordRepeatError = password === passwordRepeat ? '' : 'Does not match to password';
   }
-}
+
+  return (
+    <div className="container">
+      <h1 className="text-center">Sign Up</h1>
+      <div className="col-12 mb-3">
+        <Input
+          name="displayName"
+          label="Display Name"
+          placeholder="Your display name"
+          value={form.displayName}
+          onChange={onChange}
+          hasError={errors.displayName && true}
+          error={errors.displayName}
+        />
+      </div>
+
+      <div className="col-12 mb-3">
+        <Input
+          name="username"
+          label="Username"
+          placeholder="Your username"
+          value={form.username}
+          onChange={onChange}
+          hasError={errors.username && true}
+          error={errors.username}
+        />
+      </div>
+
+      <div className="col-12 mb-3">
+        <Input
+          name="password"
+          label="Password"
+          placeholder="Your password"
+          type="password"
+          value={form.password}
+          onChange={onChange}
+          hasError={errors.password && true}
+          error={errors.password}
+        />
+      </div>
+
+      <div className="col-12 mb-3">
+        <Input
+          name="passwordRepeat"
+          label="Password Repeat"
+          placeholder="Repeat your password"
+          type="password"
+          value={form.passwordRepeat}
+          onChange={onChange}
+          hasError={passwordRepeatError && true}
+          error={passwordRepeatError}
+        />
+      </div>
+
+      <div className="text-center">
+        <ButtonWithProgress
+          onClick={onClickSignup}
+          disabled={pendingApiCall || passwordRepeatError ? true : false}
+          pendingApiCall={pendingApiCall}
+          text="Sign Up"
+        />
+      </div>
+    </div>
+  );
+};
 
 UserSignUpPage.defaultProps = {
   actions: {
